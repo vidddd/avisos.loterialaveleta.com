330a7abeb88629f71afb89dbe34cf17a2e837ac9
vidddd <david@hipertintorero.com>
2020-08-12 14:23:52 +0200

    segunda

--- src/Controller/AvisosController.php
+++ src/Controller/AvisosController.php
@@ -2,25 +2,34 @@
 // src/Controller/AvisosController.php
 namespace App\Controller;
 
+use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class AvisosController
+class AvisosController extends AbstractController
 {
     public function index(): Response
     {
         $number = random_int(0, 100);
 
-        return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
-        );
+        return $this->render('index.html.twig', [
+            'number' => $number,
+        ]);
     }
-    
+
     public function futbol(): Response
     {
         $number = random_int(0, 100);
 
         return new Response(
-            '<html><body>Lucky number: '.$number.'</body></html>'
+            '<html><body>Lucky nuuuuuuuuuuuuuuuuuuuuuumber: ' . $number . '</body></html>'
         );
+    }
+    /**
+     * @Route("/number")
+     */
+    public function number()
+    {
+        // this looks exactly the same
     }
 }
