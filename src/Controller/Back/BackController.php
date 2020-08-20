8f9643d9f9e7c9662369c9218d764a98f2c5d579
vidddd <david@hipertintorero.com>
2020-08-20 08:13:31 +0200

    de

--- src/Controller/Back/BackController.php
+++ src/Controller/Back/BackController.php
@@ -19,4 +19,49 @@ class BackController extends AbstractController
     {
         return $this->render('back/index.html.twig');
     }
+
+    /**
+     * @Route("/back/subir", name="back_subir_pdf")
+     */
+    public function subir(Request $request, SluggerInterface $slugger)
+    {
+        $pdf = new Pdf();
+        $form = $this->createForm(PdfType::class, $pdf);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            /** @var UploadedFile $brochureFile */
+            /*$brochureFile = $form->get('brochure')->getData();
+
+            // this condition is needed because the 'brochure' field is not required
+            // so the PDF file must be processed only when a file is uploaded
+            if ($brochureFile) {
+                $originalFilename = pathinfo($brochureFile->getClientOriginalName(), PATHINFO_FILENAME);
+                // this is needed to safely include the file name as part of the URL
+                $safeFilename = $slugger->slug($originalFilename);
+                $newFilename = $safeFilename.'-'.uniqid().'.'.$brochureFile->guessExtension();
+
+                // Move the file to the directory where brochures are stored
+                try {
+                    $brochureFile->move(
+                        $this->getParameter('brochures_directory'),
+                        $newFilename
+                    );
+                } catch (FileException $e) {
+                    // ... handle exception if something happens during file upload
+                }
+
+                // updates the 'brochureFilename' property to store the PDF file name
+                // instead of its contents
+                $product->setBrochureFilename($newFilename);
+            }
+
+            // ... persist the $product variable or any other work
+
+            return $this->redirect($this->generateUrl('app_product_list'));
+            */
+        }
+
+        return $this->render('back/subir.html.twig', ['form' => $form->createView(),]);
+    }
 }
