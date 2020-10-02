7d5a17d666233162d0dd4ed5414139deb7845bf3
vidddd <david@hipertintorero.com>
2020-10-02 10:44:39 +0200

    pdfjs

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,22 @@
 "use strict";
 
-(function ($) {
+$(function () {
+  setTimeout(() => {
+    // PAra que de tiempoa que se printen los PDFs
+    var container = $(".scroll-container");
+    var clip = $(".scroll-clip"); //this is the element that scrolls
+    var list = $(".scroll-list");
+    var items = $("#container-pdfs").children();
+    var itemMax = items.eq(0).outerWidth();
+    var clipMax = clip.width();
+    var posMin = 0;
+    var posMax = items.length * itemMax;
+    var addItems = Math.ceil(clipMax / itemMax);
+    console.log(addItems);
+    list.css("width", posMax + itemMax * addItems + "px");
 
-  
-})(jQuery);
+    items.slice(0, addItems).clone(true).appendTo(list);
+
+    //this.resetPosition = 0;
+  }, 900);
+});
