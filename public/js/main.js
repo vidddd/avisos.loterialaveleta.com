1b6a01954bfca3c1b72819014e108cd177f2bcd0
vidddd <david@hipertintorero.com>
2020-10-11 10:57:29 +0200

    PAso a produccion

--- public/js/main.js
+++ public/js/main.js
@@ -1,22 +1,37 @@
 "use strict";
 
 $(function () {
-  setTimeout(() => {
-    // Esperamos un poco, Para que de tiempo a que se printen los PDFs 
-    setInterval(reordenaPdfs, 5000);
-  }, 900);
+  damePdfs();
+  setInterval(damePdfs, 8000);
 
-  function reordenaPdfs() {
-    // $('#container-pdfs').shuffle();
-    printPDFS();
-    /*
-    $(".canvas-container").each(function (i) {
-      $(this).animate({
-        'left': '50px'
-      }).animate({
-        'left': '20px'
-      }, 1000)
-    });*/
+  function damePdfs() {
+    $("#container-pdfs").empty(); // limpiamos antes de pintar el html
+    $.getJSON("damePdfs", function (pdfs) {
+      pdfs.forEach(printPDF);
+    });
   }
+  function printPDF(elemento, indice) {
+    let html = `<div id="pdf_viewer_${indice}" class="canvas-container">
+                            <canvas id="pdf_renderer_${indice}"></canvas>
+                        </div>`;
 
+    document
+      .getElementById("container-pdfs")
+      .insertAdjacentHTML("beforeend", html);
+
+    const loadPDF = pdfjsLib.getDocument("./pdfs/" + elemento).then((pdf) => {
+      //cojemos solo una pagina
+      pdf.getPage(1).then((page) => {
+        var canvas = document.getElementById("pdf_renderer_" + indice);
+        var ctx = canvas.getContext("2d");
+        var viewport = page.getViewport(1);
+        canvas.width = viewport.width;
+        canvas.height = viewport.height;
+        page.render({
+          canvasContext: ctx,
+          viewport: viewport,
+        });
+      });
+    });
+  }
 });
--- src/Controller/AvisosController.php
+++ src/Controller/AvisosController.php
@@ -6,26 +6,18 @@ use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Finder\Finder;
+use Symfony\Component\HttpFoundation\JsonResponse;
 
 class AvisosController extends AbstractController
 {
     public function index(): Response
     {
-        $pdfs = [];
-        $finder = new Finder();
-        $finder->depth('== 0');
-        $finder->files()->ignoreUnreadableDirs()->in('../public/pdfs');
-        if ($finder->hasResults()) {
-            foreach ($finder as $file) {
-                //echo $absoluteFilePath = $file->getRealPath();
-                $pdfs[] = $file->getRelativePathname();
-            }
-           shuffle($pdfs);
-        }
+        return $this->render('index.html.twig');
+    }
 
-        return $this->render('index.html.twig', [
-            'pdfs' => $pdfs,
-        ]);
+    public function pdfs()
+    {
+
     }
 
     public function futbol(): Response
@@ -36,12 +28,26 @@ class AvisosController extends AbstractController
             '<html><body>Lucky nuuuuuuuuuuuuuuuuuuuuuumber: ' . $number . '</body></html>'
         );
     }
-   
+
     /**
-     * @Route("/number")
+     * @Route("/damePdfs")
      */
-    public function number()
+    public function damePdfs()
     {
-        // this looks exactly the same
+        $pdfs = [];
+        $finder = new Finder();
+        $finder->depth('== 0');
+        $finder->files()->ignoreUnreadableDirs()->in('../public/pdfs');
+        if ($finder->hasResults()) {
+            foreach ($finder as $file) {
+                //echo $absoluteFilePath = $file->getRealPath();
+                $pdfs[] = $file->getRelativePathname();
+            }
+            shuffle($pdfs);
+        }
+
+        $response = new JsonResponse();
+        $response->setData(array_slice($pdfs, 0, 3));
+        return $response;
     }
 }
