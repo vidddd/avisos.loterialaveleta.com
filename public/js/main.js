090f125c48ed71c9432c5c8a7f1e02974c4b7811
vidddd <david@hipertintorero.com>
2020-10-12 16:45:11 +0200

    ajustes

--- public/js/main.js
+++ public/js/main.js
@@ -2,7 +2,7 @@
 
 $(function () {
   damePdfs();
-  setInterval(damePdfs, 8000);
+  var interval = setInterval(damePdfs, 10000);
 
   function damePdfs() {
     $("#container-pdfs").empty(); // limpiamos antes de pintar el html
@@ -10,6 +10,7 @@ $(function () {
       pdfs.forEach(printPDF);
     });
   }
+
   function printPDF(elemento, indice) {
     let html = `<div id="pdf_viewer_${indice}" class="canvas-container">
                             <canvas id="pdf_renderer_${indice}"></canvas>
@@ -20,18 +21,19 @@ $(function () {
       .insertAdjacentHTML("beforeend", html);
 
     const loadPDF = pdfjsLib.getDocument("./pdfs/" + elemento).then((pdf) => {
-      //cojemos solo una pagina
-      pdf.getPage(1).then((page) => {
-        var canvas = document.getElementById("pdf_renderer_" + indice);
-        var ctx = canvas.getContext("2d");
-        var viewport = page.getViewport(1);
-        canvas.width = viewport.width;
-        canvas.height = viewport.height;
-        page.render({
-          canvasContext: ctx,
-          viewport: viewport,
+          //cojemos solo una pagina
+          pdf.getPage(1).then((page) => {
+            var canvas = document.getElementById("pdf_renderer_" + indice);
+            var ctx = canvas.getContext("2d");
+            var viewport = page.getViewport(1);
+            canvas.width = viewport.width;
+            canvas.height = viewport.height;
+            page.render({
+              canvasContext: ctx,
+              viewport: viewport,
+            });
+          });
+           Promise.resolve();
         });
-      });
-    });
   }
 });
